a20d6c5379c0b63538618596dce565d4be49a543
jerlynoo <jerlynho00@gmail.com>
2024-11-01 14:58:58 +0800

    updated the venue href to be a lighter blue colour and chnaged the mr chicken. Made the cards in project page nicer and updated renderProjects function

--- js/project.js
+++ js/project.js
@@ -59,25 +59,27 @@ document.addEventListener("DOMContentLoaded", () => {
       ) {
         container.innerHTML += `
           <div class="item col-md-3 col-sm-6 col-xs-12 ${projectClass}" style="position: relative;">
-            <div class="page-preview">
-              <div class="thumb">
-                <img src="../project_images/${project.Title.toLowerCase().replace(
-                  /\s/g,
-                  "_"
-                )}.jpg" class="img-responsive" alt="${project.Title}" />
-                <div class="overlay-text"></div>
-                <div class="overlay">
-                  <a href="../project_pages/${project.Title.toLowerCase().replace(
+            <a href="../project_pages/${project.Title.toLowerCase().replace(
+              /\s/g,
+              "_"
+            )}.html" style="display: block; text-decoration: none; color: inherit;">
+              <div class="page-preview">
+                <div class="thumb">
+                  <img src="../project_images/${project.Title.toLowerCase().replace(
                     /\s/g,
                     "_"
-                  )}.html">View Page</a>
-                </div>
-                <!-- Caption -->
-                <div class="caption">
-                  <p>${project.Title}</p>
+                  )}.jpg" class="img-responsive" alt="${project.Title}" />
+                  <div class="overlay-text"></div>
+                  <div class="overlay">
+                    <span>View Page</span>
+                  </div>
+                  <!-- Caption -->
+                  <div class="caption">
+                    <p>${project.Title}</p>
+                  </div>
                 </div>
               </div>
-            </div>
+            </a>
           </div>
         `;
       }
