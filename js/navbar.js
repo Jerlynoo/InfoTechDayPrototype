c47b5274f2ec3ade42fc9ca0e835af2bc37b7187
jerlynoo <jerlynho00@gmail.com>
2024-11-01 15:54:47 +0800

    made changes to the venue info and updated the link for the iit courses

--- js/navbar.js
+++ js/navbar.js
@@ -6,7 +6,7 @@ window.addEventListener("load", function () {
       <p class="navbarP"><a class="navbarA" href="../pages/seminar.html">Seminar</a></p>
       <p class="navbarP"><a class="navbarA" href="https://forms.office.com/r/CwyyHHLfag" target="_blank">Project Voting</a></p>
       <p class="navbarP"><a class="navbarA" href="../pages/direction.html">Getting to TP</a></p>
-      <p class="navbarP"><a class="navbarA" href="../pages/courses.html">IIT Courses</a></p>
+      <p class="navbarP"><a class="navbarA" href="https://www.tp.edu.sg/schools-and-courses/students/all-diploma-courses.html" id="iit-courses-link">IIT Courses</a></p>
       <p class="navbarP"><a class="navbarA" href="https://www.tp.edu.sg/research-and-industry/centres-of-excellence/centres-under-school-of-informatics-it.html">Centers of Excellence</a></p>
     </div>
     <div class="menu__toggler"><span></span></div>
