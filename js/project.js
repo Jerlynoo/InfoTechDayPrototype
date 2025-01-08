67d479c36a8f627c52aeddd07c73e8cdf60e2238
jerlynoo <jerlynho00@gmail.com>
2025-01-08 22:44:30 +0800

    added advUNet3+ project, incomplete and updated gamification, and edited the link for proj html with data json file.

--- js/project.js
+++ js/project.js
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", () => {
       ) {
         container.innerHTML += `
           <div class="item col-md-3 col-sm-6 col-xs-12 ${projectClass}" style="position: relative;">
-            <a href="../project_pages/${project.Title.toLowerCase().replace(
+            <a href="../project_pages/${project.Page.toLowerCase().replace(
               /\s/g,
               "_"
             )}.html" style="display: block; text-decoration: none; color: inherit;">
